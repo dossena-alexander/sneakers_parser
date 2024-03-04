3152ff8b8bb817dca6362bf01ed7beb004fafb63
Nico Dillinger <writebutread@outlook.com>
2024-03-04 21:41:54 +0300

    bag fix with excel convert

--- convert_csv_to_excel.py
+++ convert_csv_to_excel.py
@@ -3,9 +3,10 @@ from scrapper import settings
 
 
 def convert_to_excel():
-    with open(f'{settings.PATH.scrapped}/products-table.csv', 'w') as f:
+    with open(f'{settings.PATH.scrapped}/products-table.csv', 'r') as f:
         df = pandas.read_csv(f)
-        df.to_excel()
+    
+    df.to_excel()
 
 if __name__ == '__main__':
     convert_to_excel()
